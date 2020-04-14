9fbdc46113ebe32d503cf0ad64e310773596e0f5
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-14 18:47:46 -0400

    Updates

---
+++ precache-manifest.d073a7b09f545adbe43e973aed04044f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dad35bd6b3d4f7b22361ebeb4aefd5",
+    "url": "/React-pics/index.html"
+  },
+  {
+    "revision": "37d3dc60aceabef73c60",
+    "url": "/React-pics/static/css/main.0ad61cf1.chunk.css"
+  },
+  {
+    "revision": "2d4bd92103f9d0f3bcaf",
+    "url": "/React-pics/static/js/2.c4df6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-pics/static/js/2.c4df6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d3dc60aceabef73c60",
+    "url": "/React-pics/static/js/main.352c4b70.chunk.js"
+  },
+  {
+    "revision": "def2f1bc6d6cc799ad8a",
+    "url": "/React-pics/static/js/runtime-main.552d8320.js"
+  }
+]);
